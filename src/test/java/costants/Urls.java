a13fc41a238deb14cd438ff100e3b2fe68458dbd
Karolina <karolina2410@mail.ru>
2022-07-16 00:05:29 +0300

    HTML tests added

---
+++ src/test/java/costants/Urls.java
@@ -0,0 +1,5 @@
+package costants;
+
+public class Urls {
+    public static final String myPage="file:///C:/Users/parhi/Desktop/задачи курсов/lesson10/BodyWeightCalculate/src/test/resources/Index.html";
+}
